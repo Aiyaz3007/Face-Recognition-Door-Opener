687fe6b07457126a4e0e60f0dc2c3c2160f55070
Aiyaz <81869013+Aiyaz3007@users.noreply.github.com>
2023-10-22 01:24:49 +0530

    Update sms_api.py

--- sms_api.py
+++ sms_api.py
@@ -1,6 +1,6 @@
 import requests
 
-def sms_api(number="6382871299"):
+def sms_api(number="6382871***"):
     
 
     msg = "Suspicious person have spotted at front of your Door."
@@ -11,7 +11,7 @@ def sms_api(number="6382871299"):
     payload = f"sender_id=TXTIND&message={msg}&route=v3&language=english&numbers={number}"
 
     headers = {
-        "authorization":"01JSwljPWurXc8sBzAkv2MdtG9NEFZi736YyUTQLbKCHRfnhDaKV9FTygp6AJmrijqouZl75BYs4XH2n",
+        "authorization":"api-key",
         "Content-Type":'application/x-www-form-urlencoded'
     }
 
